0024a4c454e5e733e5459c59d54cb256ac2ada1a
Dependability <Robloxplayer307@gmail.com>
2023-04-23 00:16:10 -0500

    Changed anchor tags to Link componenets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Icon from "@mdi/react";
 import { useState } from "react";
 import { mdiClose, mdiMenu } from "@mdi/js";
@@ -9,10 +9,10 @@ function Header() {
         <div className="inner">
         <h2 className='title' onClick={()=> {navigate(process.env.PUBLIC_URL + '/')}}>Seyi</h2>
         <div className='right'>
-            <a href={process.env.PUBLIC_URL + '/'}>Home</a>
-            <a href={process.env.PUBLIC_URL + '/about'}>About</a>
-            <a href={process.env.PUBLIC_URL + '/projects'}>Projects</a>
-            <a href={process.env.PUBLIC_URL + '/'}>Resume</a>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/'>Resume</Link>
         </div>
         <div className='menu' onClick={() => {
             setMenu(val => !val);
@@ -25,10 +25,10 @@ function Header() {
         }}>
             <div className='menuInner' onClick={(e) => {e.stopPropagation()}}>
                 <Icon path={mdiClose} size={1} onClick={()=> {setMenu(false)}}></Icon>
-                <a href={process.env.PUBLIC_URL + '/'} onClick={()=>{setMenu(false)}}>Home</a>
-                <a href={process.env.PUBLIC_URL + '/about'} onClick={()=>{setMenu(false)}}>About</a>
-                <a href={process.env.PUBLIC_URL + '/projects'} onClick={()=>{setMenu(false)}}>Projects</a>
-                <a href={process.env.PUBLIC_URL + '/'} onClick={()=>{setMenu(false)}}>Resume</a>
+                <Link to='/' onClick={()=>{setMenu(false)}}>Home</Link>
+                <Link to='/about' onClick={()=>{setMenu(false)}}>About</Link>
+                <Link to='/projects' onClick={()=>{setMenu(false)}}>Projects</Link>
+                <Link to='/' onClick={()=>{setMenu(false)}}>Resume</Link>
                 <div className='line'></div>
             </div>
         </div> : ''}
